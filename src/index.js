ea52fdc08dcc5d929f3b4b95262fa455b254862c
Raman Karki <ramankarki40@gmail.com>
2021-08-16 19:50:57 +0545

    setup: hot reloading, redux Provider, store, etc.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import App from './App';
+import configureStore from './configureStore';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = configureStore({});
 
-serviceWorkerRegistration.register();
+// hot reloading - only reset the code changed reducer, https://redux.js.org/usage/configuring-your-store#hot-reloading
+const renderApp = () =>
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
+
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept('./App', renderApp);
+}
+
+renderApp();
+
+if (process.env.NODE_ENV === 'production') serviceWorkerRegistration.register();
+serviceWorkerRegistration.unregister();
