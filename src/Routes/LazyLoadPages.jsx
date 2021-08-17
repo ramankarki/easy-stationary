2e4ffbdd695c28e1b7a5d598bc8e721172512a3e
Raman Karki <ramankarki40@gmail.com>
2021-08-17 17:06:21 +0545

    simple setup to return object where key is page component name and value is component itself

---
+++ src/Routes/LazyLoadPages.jsx
@@ -0,0 +1,26 @@
+import { lazy, Suspense } from 'react';
+
+import PageLoadingSpinner from '../components/PageLoadingSpinner';
+import ErrorBoundary from '../components/ErrorBoundary';
+
+// Add pages here
+const PAGES = ['Landing', 'AllProducts', 'Signup', 'PageNotFound'];
+
+function LazyLoadPages(page) {
+  const Component = lazy(() => import(`../pages/${page}`));
+  return () => (
+    <ErrorBoundary>
+      <Suspense fallback={PageLoadingSpinner()}>
+        <Component />
+      </Suspense>
+    </ErrorBoundary>
+  );
+}
+
+// Generate key (page): value (lazy loading component with error boundary) pair.
+const COMPONENTS = {};
+PAGES.forEach((page) => {
+  COMPONENTS[page] = LazyLoadPages(page);
+});
+
+export default COMPONENTS;
