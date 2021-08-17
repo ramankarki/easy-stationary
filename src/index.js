0bb30dcbea62cf803b56b7c8afc047ab7380a1a1
Raman Karki <ramankarki40@gmail.com>
2021-08-17 18:37:51 +0545

    dynamic import react-axe in development

--- src/index.js
+++ src/index.js
@@ -10,7 +10,18 @@ const store = configureStore({});
 
 // hot reloading - only reset the code changed reducer, https://redux.js.org/usage/configuring-your-store#hot-reloading
 const renderApp = () => {
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV !== 'production') {
+    // enable accessibility debugging with react-axe
+    import('react-axe').then((axe) => {
+      axe.default(React, ReactDOM, 1000);
+      ReactDOM.render(
+        <Provider store={store}>
+          <App />
+        </Provider>,
+        document.getElementById('root')
+      );
+    });
+  } else {
     ReactDOM.render(
       <Provider store={store}>
         <App />
@@ -18,17 +29,6 @@ const renderApp = () => {
       document.getElementById('root')
     );
   }
-
-  // enable accessibility debugging with react-axe
-  import('react-axe').then((axe) => {
-    axe.default(React, ReactDOM, 1000);
-    ReactDOM.render(
-      <Provider store={store}>
-        <App />
-      </Provider>,
-      document.getElementById('root')
-    );
-  });
 };
 
 if (process.env.NODE_ENV !== 'production' && module.hot) {
