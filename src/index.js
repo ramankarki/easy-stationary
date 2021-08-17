37ecd6dc6a3bb9641dbc3f88075537b7a4ee8356
Raman Karki <ramankarki40@gmail.com>
2021-08-17 11:26:50 +0545

    react-axe setup for accessibility debugging

--- src/index.js
+++ src/index.js
@@ -9,19 +9,33 @@ import configureStore from './configureStore';
 const store = configureStore({});
 
 // hot reloading - only reset the code changed reducer, https://redux.js.org/usage/configuring-your-store#hot-reloading
-const renderApp = () =>
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root')
-  );
+const renderApp = () => {
+  if (process.env.NODE_ENV === 'production') {
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      document.getElementById('root')
+    );
+  }
+
+  // enable accessibility debugging with react-axe
+  import('react-axe').then((axe) => {
+    axe.default(React, ReactDOM, 1000);
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      document.getElementById('root')
+    );
+  });
+};
 
 if (process.env.NODE_ENV !== 'production' && module.hot) {
   module.hot.accept('./App', renderApp);
 }
-
 renderApp();
 
+// enable service worker on production
 if (process.env.NODE_ENV === 'production') serviceWorkerRegistration.register();
 serviceWorkerRegistration.unregister();
