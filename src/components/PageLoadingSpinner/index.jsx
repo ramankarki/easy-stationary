faf38b0326033ea04e368f7d687a546416ba3b41
Raman Karki <ramankarki40@gmail.com>
2021-08-17 13:23:25 +0545

    page loading indicator

---
+++ src/components/PageLoadingSpinner/index.jsx
@@ -0,0 +1,19 @@
+import './index.scss';
+
+export default function PageLoadingSpinner() {
+  return (
+    <div className="pageLoadingBody">
+      <div class="cubes">
+        <div class="cube cube-1"></div>
+        <div class="cube cube-2"></div>
+        <div class="cube cube-3"></div>
+        <div class="cube cube-4"></div>
+        <div class="cube cube-5"></div>
+        <div class="cube cube-6"></div>
+        <div class="cube cube-7"></div>
+        <div class="cube cube-8"></div>
+        <div class="cube cube-9"></div>
+      </div>
+    </div>
+  );
+}
