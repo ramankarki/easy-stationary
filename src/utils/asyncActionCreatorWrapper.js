a271377fe19ba4f328955bc88a89a078b04d7bbb
Raman Karki <ramankarki40@gmail.com>
2021-08-23 19:52:22 +0545

    remove unused file

--- src/utils/asyncActionCreatorWrapper.js
+++
@@ -1,6 +0,0 @@
-import errorHandler from '../actions/errorHandler';
-
-const asyncWrapper = (fn) => (dispatch, getState) =>
-  fn(dispatch, getState).catch((err) => errorHandler(err, dispatch, getState));
-
-export default asyncWrapper;
