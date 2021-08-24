04903224e8f4a999edbf46e05134a7b801729b16
Raman Karki <ramankarki40@gmail.com>
2021-08-24 22:12:01 +0545

    page setup for login with FormGenerator

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,36 @@
-import './login.scss';
+import { connect } from 'react-redux';
+
+import { SIGNUP } from '../../Routes/contants';
+import { UI_LOGIN_STATE, APP_LOGIN_STATE, USER } from '../../actions/constants';
+import fields from '../../utils/fields';
+import { injectReducer } from '../../utils/dynamicReducers';
+import HOFreducer from '../../reducers/HOFreducer';
+
+import FormGenerator from '../../templates/FormGenerator';
 
 function Login(props) {
-  return <h1>login page</h1>;
+  const { REDUX_APP_STATE } = props;
+  const loginFields = fields('Email', 'Password');
+
+  injectReducer(USER, HOFreducer(USER, {}));
+
+  return (
+    <FormGenerator
+      formHeading="Login"
+      buttonValue="Login"
+      fieldsObj={loginFields}
+      UI_STATE={UI_LOGIN_STATE}
+      APP_STATE={APP_LOGIN_STATE}
+      REDUX_APP_STATE={REDUX_APP_STATE}
+      redirect={SIGNUP}
+      redirectMsg="New to Easy Stationary? Create Account"
+      topLogo
+    />
+  );
 }
 
-export default Login;
+const mapStateToProps = (state) => {
+  return { REDUX_APP_STATE: state[APP_LOGIN_STATE] || {} };
+};
+
+export default connect(mapStateToProps, {})(Login);
