d90f56d1e08b7485056cd79bbf28a7b1cd082078
Raman Karki <ramankarki40@gmail.com>
2021-08-24 10:04:16 +0545

    cover img with button and add onClick event listner to exit modal

--- src/templates/RequestStatusModalBg/index.jsx
+++ src/templates/RequestStatusModalBg/index.jsx
@@ -5,7 +5,14 @@ import './requestStatusModalBg.scss';
 function RequestStatusModalBg(props) {
   return createPortal(
     <div className="modalBg">
-      <div className="modalBg__wrapper">{props.children}</div>
+      <div className="modalBg__wrapper">
+        {props.requestStatus === 'failed' && (
+          <button className="modalBg__exit" onClick={props.onExit}>
+            <img src="/assets/exit icon.svg" alt="exit icon" />
+          </button>
+        )}
+        {props.children}
+      </div>
     </div>,
     document.getElementById('modal')
   );
