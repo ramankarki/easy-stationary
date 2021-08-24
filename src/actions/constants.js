2b74986ae8d490492c634ed3745b941cc8aa348f
Raman Karki <ramankarki40@gmail.com>
2021-08-24 19:39:30 +0545

    add UI and APP constants for activate account form page

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,10 +1,18 @@
-// UI state
+/**
+ * @description these state will be used in UI.
+ */
 export const UI_SIGNUP_STATE = 'UI_SIGNUP_STATE';
 export const UI_LOGIN_STATE = 'UI_LOGIN_STATE';
+export const UI_ACTIVATE_ACCOUNT_STATE = 'UI_ACTIVATE_ACCOUNT_STATE';
 
-// app state
+/**
+ * @description these state will used to track data like ajax calls behind the seen.
+ */
 export const APP_SIGNUP_STATE = 'APP_SIGNUP_STATE';
 export const APP_LOGIN_STATE = 'APP_LOGIN_STATE';
+export const APP_ACTIVATE_ACCOUNT_STATE = 'APP_ACTIVATE_ACCOUNT_STATE';
 
-// domain data
+/**
+ * @description data received from server will be stored here.
+ */
 export const USER = 'USER';
