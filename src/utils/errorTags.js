013d161ee88dc39698d2018d5773a62f2b7d9ca2
Raman Karki <ramankarki40@gmail.com>
2021-08-24 10:39:58 +0545

    collection of error tags with jsx attached, add alreadyExists, alreadyExistsNotActive tag with jsx

---
+++ src/utils/errorTags.js
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom';
+
+import { ACTIVATE_ACCOUNT, LOGIN } from '../Routes/contants';
+
+import LazyImg from '../components/LazyImg';
+
+const errorTags = {
+  accountAlreadyActive: 'accountAlreadyActive',
+  accountNotActive: 'accountNotActive',
+  alreadyExistsNotActive: (
+    <div className="signup__modalMsg">
+      <picture>
+        <LazyImg src="/assets/user icon.svg" alt="user icon" />
+      </picture>
+      <p>
+        User account with this email already exists but is not active.{' '}
+        <Link to={ACTIVATE_ACCOUNT}>Activate now</Link>
+      </p>
+    </div>
+  ),
+  alreadyExists: (
+    <div className="signup__modalMsg">
+      <picture>
+        <LazyImg src="/assets/error icon.svg" alt="user icon" />
+      </picture>
+      <p>
+        User account with this email already exists.{' '}
+        <Link to={LOGIN}>Login</Link>
+      </p>
+    </div>
+  ),
+  activationTokenExpired: 'activationTokenExpired',
+  resetPasswordTokenExpired: 'resetPasswordTokenExpired',
+  noUserWithEmail: 'noUserWithEmail',
+  wrongEmailOrPassword: 'wrongEmailOrPassword',
+  userDoesntExistAnymore: 'userDoesntExistAnymore',
+  productQuantityOutOfStock: 'productQuantityOutOfStock',
+  buyProductToGiveReview: 'buyProductToGiveReview',
+  emailUpdateTokenExpired: 'emailUpdateTokenExpired',
+  incorrectPassword: 'incorrectPassword',
+  logout: 'logout',
+  invalidToken: 'invalidToken',
+};
+
+const getErrorTag = (errorTag) =>
+  errorTags[errorTag] || <p>Something went wrong! Please try again later</p>;
+export default getErrorTag;
