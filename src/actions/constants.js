2674d14bd63bb520978d1564346fec96966aff30
Raman Karki <ramankarki40@gmail.com>
2021-08-24 18:47:25 +0545

    add constants for login UI and APP state

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,7 +1,9 @@
 // UI state
 export const UI_SIGNUP_STATE = 'UI_SIGNUP_STATE';
+export const UI_LOGIN_STATE = 'UI_LOGIN_STATE';
 
 // app state
 export const APP_SIGNUP_STATE = 'APP_SIGNUP_STATE';
+export const APP_LOGIN_STATE = 'APP_LOGIN_STATE';
 
 // domain data
