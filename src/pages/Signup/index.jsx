916fff459eeba5af8e4c95e0a4ea41d572d66a79
Raman Karki <ramankarki40@gmail.com>
2021-08-24 18:48:38 +0545

    move common code for forms pages to FormGenerator

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,25 +1,13 @@
-import { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 import { LOGIN } from '../../Routes/contants';
-import { injectReducer, ejectReducer } from '../../utils/dynamicReducers';
 import { UI_SIGNUP_STATE, APP_SIGNUP_STATE } from '../../actions/constants';
-import onSubmit from '../../actions/onSubmit';
-import resetAppState from '../../actions/resetAppState';
-import HOFreducer from '../../reducers/HOFreducer';
 import fields from '../../utils/fields';
-import appState from '../../utils/appState';
 
-import LazyImg from '../../components/LazyImg';
-import AuthFormElements from '../../templates/AuthFormElements';
-import Button from '../../components/Button';
-import RequestStatusModalBg from '../../templates/RequestStatusModalBg';
-import SpinnerLoading from '../../components/SpinnerLoading';
-import './signup.scss';
+import FormGenerator from '../../templates/FormGenerator';
 
 function Signup(props) {
-  const { requestStatus, modalMsg, errorTag } = props;
+  const { REDUX_APP_STATE } = props;
   const signupFields = fields(
     'First name',
     'Last name',
@@ -30,70 +18,23 @@ function Signup(props) {
     'Re-type password'
   );
 
-  useEffect(() => {
-    injectReducer(UI_SIGNUP_STATE, HOFreducer(UI_SIGNUP_STATE, signupFields));
-    injectReducer(
-      APP_SIGNUP_STATE,
-      HOFreducer(APP_SIGNUP_STATE, appState(APP_SIGNUP_STATE))
-    );
-    return () => {
-      ejectReducer(UI_SIGNUP_STATE);
-      ejectReducer(APP_SIGNUP_STATE);
-    };
-  }, []);
-
-  const onSubmitHandler = (event) => {
-    event.preventDefault();
-    props.onSubmit(APP_SIGNUP_STATE, UI_SIGNUP_STATE);
-  };
-
-  const onModalExit = () =>
-    props.resetAppState(APP_SIGNUP_STATE, appState(APP_SIGNUP_STATE));
-
-  const LinkStyle = {
-    textAlign: 'center',
-    marginTop: '2rem',
-    fontSize: '0.75rem',
-    fontWeight: '600',
-    color: '#2b4775',
-  };
-
   return (
-    <div className="signup">
-      <picture className="signup__logo">
-        <LazyImg
-          src="/assets/Easy Stationary Logo.webp"
-          alt="Easy Stationary Logo"
-        />
-      </picture>
-      <section className="signup__section">
-        <h1 className="signup__heading">Create account</h1>
-        <form onSubmit={onSubmitHandler} className="signup__form">
-          <AuthFormElements fields={signupFields} TYPE={UI_SIGNUP_STATE} />
-          <Button value="Create account" />
-        </form>
-        <Link to={LOGIN} style={LinkStyle}>
-          Already have an account? Login
-        </Link>
-      </section>
-      {requestStatus && (
-        <RequestStatusModalBg
-          requestStatus={requestStatus}
-          onExit={onModalExit}
-        >
-          {requestStatus === 'pending' ? (
-            <SpinnerLoading />
-          ) : (
-            modalMsg(requestStatus, errorTag)
-          )}
-        </RequestStatusModalBg>
-      )}
-    </div>
+    <FormGenerator
+      formHeading="Create Account"
+      buttonValue="Create Account"
+      fieldsObj={signupFields}
+      UI_STATE={UI_SIGNUP_STATE}
+      APP_STATE={APP_SIGNUP_STATE}
+      REDUX_APP_STATE={REDUX_APP_STATE}
+      redirect={LOGIN}
+      redirectMsg="Already have an account? Login"
+      topLogo
+    />
   );
 }
 
 const mapStateToProps = (state) => {
-  return state[APP_SIGNUP_STATE] || {};
+  return { REDUX_APP_STATE: state[APP_SIGNUP_STATE] || {} };
 };
 
-export default connect(mapStateToProps, { onSubmit, resetAppState })(Signup);
+export default connect(mapStateToProps, {})(Signup);
