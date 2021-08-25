78c85bbe725eb6d6e4e528487713a58fb64353af
Raman Karki <ramankarki40@gmail.com>
2021-08-25 14:01:23 +0545

    app state for forgot and reset password

--- src/utils/appState.js
+++ src/utils/appState.js
@@ -5,16 +5,19 @@ import {
   APP_LOGIN_STATE,
   USER,
   APP_ACTIVATE_ACCOUNT_STATE,
+  APP_FORGOT_PASSWORD_STATE,
+  APP_RESET_PASSWORD_STATE,
 } from '../actions/constants';
-import { ALL_PRODUCTS } from '../Routes/contants';
+import { ALL_PRODUCTS, RESET_PASSWORD } from '../Routes/contants';
 import getErrorTag from './errorTags';
+import queryString from './getQueryString';
 
 import LazyImg from '../components/LazyImg';
 
 const appState = {
   [APP_SIGNUP_STATE]: {
-    apiPath: (path = ALL_PRODUCTS) =>
-      `/api/v1/auth/signup?redirect=${window.location.origin}/%23${path}`,
+    apiPath: (redirectPath = ALL_PRODUCTS) =>
+      `/api/v1/auth/signup?redirect=${window.location.origin}/%23${redirectPath}`,
     modalMsg: (requestStatus, errorTag) => {
       if (requestStatus === 'failed') return getErrorTag(errorTag);
       return (
@@ -43,8 +46,8 @@ const appState = {
     domainState: USER,
   },
   [APP_ACTIVATE_ACCOUNT_STATE]: {
-    apiPath: (path = ALL_PRODUCTS) =>
-      `/api/v1/auth/resend-activation-link?redirect=${window.location.origin}/%23${path}`,
+    apiPath: (redirectPath = ALL_PRODUCTS) =>
+      `/api/v1/auth/resend-activation-link?redirect=${window.location.origin}/%23${redirectPath}`,
     modalMsg: (requestStatus, errorTag) => {
       if (requestStatus === 'failed') return getErrorTag(errorTag);
       return (
@@ -62,6 +65,35 @@ const appState = {
           </p>
         </div>
       );
+    },
+  },
+  [APP_FORGOT_PASSWORD_STATE]: {
+    apiPath: (redirectPath = ALL_PRODUCTS) =>
+      `/api/v1/auth/forgot-password?reset-password-page=${window.location.origin}/%23${RESET_PASSWORD}&redirect=${window.location.origin}/%23${redirectPath}`,
+    modalMsg: (requestStatus, errorTag) => {
+      if (requestStatus === 'failed') return getErrorTag(errorTag);
+      return (
+        <div className="Form__modalMsg">
+          <picture>
+            <LazyImg src="/assets/check.svg" alt="success icon" />
+          </picture>
+          <p>
+            Email has been sent! Please{' '}
+            <a href="https://gmail.com" target="_blank" rel="noreferrer">
+              confirm
+            </a>{' '}
+            your account before you reset your password. (Do check your
+            promotions tab if you don't see any email at inbox)
+          </p>
+        </div>
+      );
+    },
+  },
+  [APP_RESET_PASSWORD_STATE]: {
+    apiPath: () => `/api/v1/auth/reset-password/${queryString().token}`,
+    modalMsg: (requestStatus, errorTag) => {
+      if (requestStatus === 'failed') return getErrorTag(errorTag);
+      return <Redirect to={queryString().redirect.split('#')[1]} />;
     },
   },
 };
