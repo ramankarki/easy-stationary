84ac553cda7c751c350eda45857a57af01a7641a
Raman Karki <ramankarki40@gmail.com>
2021-08-25 10:29:28 +0545

    setup forgot password page with FormGenerator template

---
+++ src/pages/ForgotPassword/index.jsx
@@ -0,0 +1,37 @@
+import { connect } from 'react-redux';
+
+import {
+  UI_FORGOT_PASSWORD_STATE,
+  APP_FORGOT_PASSWORD_STATE,
+  USER,
+} from '../../actions/constants';
+import fields from '../../utils/fields';
+import { injectReducer } from '../../utils/dynamicReducers';
+import HOFreducer from '../../reducers/HOFreducer';
+
+import FormGenerator from '../../templates/FormGenerator';
+
+function Login(props) {
+  const { REDUX_APP_STATE } = props;
+  const loginFields = fields('Email');
+
+  injectReducer(USER, HOFreducer(USER, {}));
+
+  return (
+    <FormGenerator
+      formHeading="Forgot Password"
+      buttonValue="Send Email"
+      fieldsObj={loginFields}
+      UI_STATE={UI_FORGOT_PASSWORD_STATE}
+      APP_STATE={APP_FORGOT_PASSWORD_STATE}
+      REDUX_APP_STATE={REDUX_APP_STATE}
+      topLogo
+    />
+  );
+}
+
+const mapStateToProps = (state) => {
+  return { REDUX_APP_STATE: state[APP_FORGOT_PASSWORD_STATE] || {} };
+};
+
+export default connect(mapStateToProps, {})(Login);
