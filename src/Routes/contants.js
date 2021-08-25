56f0466222db93eefdf612231d252065c636d222
Raman Karki <ramankarki40@gmail.com>
2021-08-25 15:35:28 +0545

    add admin panel routes

--- src/Routes/contants.js
+++ src/Routes/contants.js
@@ -6,4 +6,13 @@ export const ACTIVATE_ACCOUNT = '/auth/activate-account';
 export const FORGOT_PASSWORD = '/auth/forgot-password';
 export const RESET_PASSWORD = '/auth/reset-password';
 export const ALL_PRODUCTS = '/all-products';
+
+// admin routes start
+export const ADMIN = '/admin';
+export const ADMIN_CATEGORY = '/admin/category';
+export const ADMIN_ADD_NEW_PRODUCT = '/admin/add-new-product';
+export const ADMIN_ORDERS = '/admin/orders';
+export const ADMIN_settings = '/admin/settings';
+// admin routes end
+
 export const PAGE_NOT_FOUND = '*';
