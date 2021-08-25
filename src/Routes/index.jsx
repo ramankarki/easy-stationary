8e5846b921958b241e0d038900ee01b6cb43c74f
Raman Karki <ramankarki40@gmail.com>
2021-08-25 16:55:27 +0545

    wrap private page inside Protect component

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,13 +1,21 @@
 import { Route, HashRouter, Switch } from 'react-router-dom';
+
 import routes from './routes';
+
+import Protect from './Protect';
 
 export default function RoutesGenerator() {
   const allTheRoutes = [];
 
   for (let path in routes) {
-    allTheRoutes.push(
+    const route = (
       <Route path={path} exact component={routes[path].component} key={path} />
     );
+
+    if (routes[path].private) {
+      allTheRoutes.push(<Protect component={route} />);
+    }
+    allTheRoutes.push(route);
   }
 
   return (
