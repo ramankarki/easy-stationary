421969a655318f95807b484b4d5573d43518dd54
Raman Karki <ramankarki40@gmail.com>
2021-08-25 15:40:04 +0545

    update variable name for admin settings route

--- src/Routes/contants.js
+++ src/Routes/contants.js
@@ -12,7 +12,7 @@ export const ADMIN = '/admin';
 export const ADMIN_CATEGORY = '/admin/category';
 export const ADMIN_ADD_NEW_PRODUCT = '/admin/add-new-product';
 export const ADMIN_ORDERS = '/admin/orders';
-export const ADMIN_settings = '/admin/settings';
+export const ADMIN_SETTINGS = '/admin/settings';
 // admin routes end
 
 export const PAGE_NOT_FOUND = '*';
