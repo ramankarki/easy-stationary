924706792aaf517dd7488dfc08771ddaa06122ad
Raman Karki <ramankarki40@gmail.com>
2021-08-25 10:28:10 +0545

    add forgot and reset password page constants

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -4,6 +4,8 @@
 export const UI_SIGNUP_STATE = 'UI_SIGNUP_STATE';
 export const UI_LOGIN_STATE = 'UI_LOGIN_STATE';
 export const UI_ACTIVATE_ACCOUNT_STATE = 'UI_ACTIVATE_ACCOUNT_STATE';
+export const UI_FORGOT_PASSWORD_STATE = 'UI_FORGOT_PASSWORD_STATE';
+export const UI_RESET_PASSWORD_STATE = 'UI_RESET_PASSWORD_STATE';
 
 /**
  * @description these state will used to track data like ajax calls behind the seen.
@@ -11,6 +13,8 @@ export const UI_ACTIVATE_ACCOUNT_STATE = 'UI_ACTIVATE_ACCOUNT_STATE';
 export const APP_SIGNUP_STATE = 'APP_SIGNUP_STATE';
 export const APP_LOGIN_STATE = 'APP_LOGIN_STATE';
 export const APP_ACTIVATE_ACCOUNT_STATE = 'APP_ACTIVATE_ACCOUNT_STATE';
+export const APP_FORGOT_PASSWORD_STATE = 'APP_FORGOT_PASSWORD_STATE';
+export const APP_RESET_PASSWORD_STATE = 'APP_RESET_PASSWORD_STATE';
 
 /**
  * @description data received from server will be stored here.
