7ec4d6034b366ee6f66b903cb065cfff4b0e1992
Raman Karki <ramankarki40@gmail.com>
2021-08-26 09:55:44 +0545

    add all products state

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -6,6 +6,7 @@ export const UI_LOGIN_STATE = 'UI_LOGIN_STATE';
 export const UI_ACTIVATE_ACCOUNT_STATE = 'UI_ACTIVATE_ACCOUNT_STATE';
 export const UI_FORGOT_PASSWORD_STATE = 'UI_FORGOT_PASSWORD_STATE';
 export const UI_RESET_PASSWORD_STATE = 'UI_RESET_PASSWORD_STATE';
+export const UI_ALL_PRODUCTS_STATE = 'UI_ALL_PRODUCTS_STATE';
 
 /**
  * @description these state will used to track data like ajax calls behind the seen.
@@ -15,6 +16,7 @@ export const APP_LOGIN_STATE = 'APP_LOGIN_STATE';
 export const APP_ACTIVATE_ACCOUNT_STATE = 'APP_ACTIVATE_ACCOUNT_STATE';
 export const APP_FORGOT_PASSWORD_STATE = 'APP_FORGOT_PASSWORD_STATE';
 export const APP_RESET_PASSWORD_STATE = 'APP_RESET_PASSWORD_STATE';
+export const APP_ALL_PRODUCTS_STATE = 'APP_ALL_PRODUCTS_STATE';
 
 /**
  * @description data received from server will be stored here.
