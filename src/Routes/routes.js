d0ae63094e207515a2034987340f4c50c3300ba8
Raman Karki <ramankarki40@gmail.com>
2021-08-26 14:36:42 +0545

    add role field in each route

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -16,46 +16,55 @@ const routes = {
   [ROOT]: {
     private: false,
     component: LazyLoadPages.Landing,
+    role: ['client', 'admin'],
   },
 
   [SIGNUP]: {
     private: false,
     component: LazyLoadPages.Signup,
+    role: ['client'],
   },
 
   [LOGIN]: {
     private: false,
     component: LazyLoadPages.Login,
+    role: ['client', 'admin'],
   },
 
   [ACTIVATE_ACCOUNT]: {
     private: false,
     component: LazyLoadPages.ActivateAccount,
+    role: ['client'],
   },
 
   [FORGOT_PASSWORD]: {
     private: false,
     component: LazyLoadPages.ForgotPassword,
+    role: ['client', 'admin'],
   },
 
   [RESET_PASSWORD]: {
     private: false,
     component: LazyLoadPages.ResetPassword,
+    role: ['client', 'admin'],
   },
 
   [ALL_PRODUCTS]: {
     private: false,
     component: LazyLoadPages.AllProducts,
+    role: ['client', 'admin'],
   },
 
   [ADMIN]: {
     private: true,
     component: LazyLoadPages.Admin,
+    role: ['admin'],
   },
 
   [PAGE_NOT_FOUND]: {
     private: false,
     component: LazyLoadPages.PageNotFound,
+    role: ['client', 'admin'],
   },
 };
 
