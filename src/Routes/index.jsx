65ddd18f0a6cfffb1fc56a4ffcf712fd01dd5d45
Raman Karki <ramankarki40@gmail.com>
2021-08-26 14:59:16 +0545

    restrict to access page by user role

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -2,7 +2,7 @@ import { Route, HashRouter, Switch, Redirect } from 'react-router-dom';
 
 import { injectReducer } from '../utils/dynamicReducers';
 import HOFreducer from '../reducers/HOFreducer';
-import { LOGIN } from './contants';
+import { LOGIN, ALL_PRODUCTS } from './contants';
 import routes from './routes';
 
 const USER = localStorage.getItem('USER');
@@ -15,15 +15,14 @@ export default function RoutesGenerator() {
   const allTheRoutes = [];
 
   for (let path in routes) {
-    let component = wrapper(
-      parsedUser?.expiryDate,
+    let page = wrapper(
+      parsedUser,
       routes[path].private,
+      routes[path].role,
       routes[path].component
     );
 
-    allTheRoutes.push(
-      <Route key={path} path={path} exact component={component} />
-    );
+    allTheRoutes.push(<Route key={path} path={path} exact component={page} />);
   }
 
   return (
@@ -33,10 +32,16 @@ export default function RoutesGenerator() {
   );
 }
 
-function wrapper(expiryDate, privateRoute, component) {
+function wrapper(parsedUser, privateRoute, role, page) {
+  // if page is accessible by user type.
+  if (!role.includes(parsedUser?.user.role))
+    return <Redirect to={ALL_PRODUCTS} />;
+
+  // check if this page needs authentication.
   if (privateRoute) {
-    if (Date.now() < expiryDate) return component;
+    // check if token is expired.
+    if (Date.now() < parsedUser?.expiryDate) return page;
     return <Redirect to={LOGIN} />;
   }
-  return component;
+  return page;
 }
