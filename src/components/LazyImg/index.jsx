d6077d859eef373ee8ab840576e6415039ba15dc
Raman Karki <ramankarki40@gmail.com>
2021-08-26 14:29:07 +0545

    change div to picture element and add source element for favicon

--- src/components/LazyImg/index.jsx
+++ src/components/LazyImg/index.jsx
@@ -5,7 +5,7 @@ import ShimmerLoading from '../ShimmerLoading';
 import ErrorBoundary from '../ErrorBoundary';
 
 const LazyImg = (props) => {
-  const { src, alt } = props;
+  const { src, alt, logo } = props;
   const [isLoaded, setIsLoaded] = useState(false);
   const [isInView, setIsInView] = useState(false);
   const imgRef = useRef();
@@ -21,10 +21,11 @@ const LazyImg = (props) => {
   // image onLoad, unmount loading component
   return (
     <ErrorBoundary>
-      <div
+      <picture
         ref={imgRef}
         style={{ height: '100%', width: '100%', position: 'relative' }}
       >
+        {logo && <source media="(max-width: 479px)" srcSet="favicon.png" />}
         {isInView && (
           <img
             src={src}
@@ -38,7 +39,7 @@ const LazyImg = (props) => {
           // when image loads, unmount shimmer loading
           !isLoaded && <ShimmerLoading />
         }
-      </div>
+      </picture>
     </ErrorBoundary>
   );
 };
