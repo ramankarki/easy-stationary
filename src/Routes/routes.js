5431d762eb4c59cf2f63083cccdbb967625e1c39
Raman Karki <ramankarki40@gmail.com>
2021-08-26 16:46:15 +0545

    private routes will only have role field

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -16,43 +16,36 @@ const routes = {
   [ROOT]: {
     private: false,
     component: LazyLoadPages.Landing,
-    role: ['client', 'admin'],
   },
 
   [SIGNUP]: {
     private: false,
     component: LazyLoadPages.Signup,
-    role: ['client'],
   },
 
   [LOGIN]: {
     private: false,
     component: LazyLoadPages.Login,
-    role: ['client', 'admin'],
   },
 
   [ACTIVATE_ACCOUNT]: {
     private: false,
     component: LazyLoadPages.ActivateAccount,
-    role: ['client'],
   },
 
   [FORGOT_PASSWORD]: {
     private: false,
     component: LazyLoadPages.ForgotPassword,
-    role: ['client', 'admin'],
   },
 
   [RESET_PASSWORD]: {
     private: false,
     component: LazyLoadPages.ResetPassword,
-    role: ['client', 'admin'],
   },
 
   [ALL_PRODUCTS]: {
     private: false,
     component: LazyLoadPages.AllProducts,
-    role: ['client', 'admin'],
   },
 
   [ADMIN]: {
@@ -64,7 +57,6 @@ const routes = {
   [PAGE_NOT_FOUND]: {
     private: false,
     component: LazyLoadPages.PageNotFound,
-    role: ['client', 'admin'],
   },
 };
 
