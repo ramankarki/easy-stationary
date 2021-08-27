00a1fd8b549fe7b5f4cea60cb6b83fae03b23077
Raman Karki <ramankarki40@gmail.com>
2021-08-27 20:41:08 +0545

    add authorization headers

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,3 +1,8 @@
 import { create } from 'axios';
 
-export default create({ baseURL: process.env.REACT_APP_API_1 });
+export default create({
+  baseURL: process.env.REACT_APP_API_1,
+  headers: {
+    authorization: 'Bearer ' + JSON.parse(localStorage.getItem('USER')).token,
+  },
+});
