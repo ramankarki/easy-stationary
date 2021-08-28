461a81ed8385f0c8f752f79041820ed06f1e7dc2
Raman Karki <ramankarki40@gmail.com>
2021-08-28 10:01:44 +0545

    admin page template for common header, breadcrumb and aside

---
+++ src/templates/AdminPageTemplate/index.jsx
@@ -0,0 +1,67 @@
+import getAside from '../../utils/asideButtonsData';
+
+import Header from '../../templates/Header';
+import BreadCrumb from '../../components/BreadCrumb';
+import RequestStatusModalBg from '../../templates/RequestStatusModalBg';
+import SpinnerLoading from '../../components/SpinnerLoading';
+import LinkButton from '../../components/LinkButton';
+
+import './adminPageTemplate.scss';
+
+function Admin(props) {
+  const { requestStatus, modalMsg, errorTag, heading } = props;
+
+  const aside = getAside(
+    'Dashboard',
+    'Category',
+    'Add new product',
+    'Orders',
+    'Settings'
+  );
+
+  const currentPath = window.location.hash.slice(1);
+
+  return (
+    <div className="admin">
+      <Header />
+      <BreadCrumb />
+
+      <section className="admin__sectionCover">
+        <aside>
+          {aside.map(({ iconsrc, value, path }) => (
+            <LinkButton
+              key={iconsrc}
+              to={path}
+              iconsrc={iconsrc}
+              alt={iconsrc}
+              nobg={currentPath !== path ? 'true' : ''}
+              bgonhover={'true'}
+              style={{ padding: '1rem', fontSize: '.9rem' }}
+            >
+              {value}
+            </LinkButton>
+          ))}
+        </aside>
+
+        {/* right panel */}
+        <div className="panel">
+          <h1>{heading}</h1>
+          {props.children}
+        </div>
+      </section>
+
+      {/* modal */}
+      {requestStatus && (
+        <RequestStatusModalBg requestStatus={requestStatus}>
+          {requestStatus === 'pending' ? (
+            <SpinnerLoading />
+          ) : (
+            modalMsg(requestStatus, errorTag)
+          )}
+        </RequestStatusModalBg>
+      )}
+    </div>
+  );
+}
+
+export default Admin;
