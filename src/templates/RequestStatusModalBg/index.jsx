c94e51e9c41e4e6cfa207bfb46d744aa38a15c51
Raman Karki <ramankarki40@gmail.com>
2021-08-29 19:10:56 +0545

    setup to exit modal with action creator

--- src/templates/RequestStatusModalBg/index.jsx
+++ src/templates/RequestStatusModalBg/index.jsx
@@ -1,13 +1,20 @@
 import { createPortal } from 'react-dom';
+import { connect } from 'react-redux';
+
+import resetAppState from '../../actions/resetAppState';
+import appState from '../../appState';
 
 import './requestStatusModalBg.scss';
 
 function RequestStatusModalBg(props) {
+  const { APP_STATE } = props;
+  const onModalExit = () => props.resetAppState(APP_STATE, appState(APP_STATE));
+
   return createPortal(
     <div className="modalBg">
       <div className="modalBg__wrapper">
         {props.requestStatus === 'failed' && (
-          <button className="modalBg__exit" onClick={props.onExit}>
+          <button className="modalBg__exit" onClick={onModalExit}>
             <img src="/assets/exit icon.svg" alt="exit icon" />
           </button>
         )}
@@ -18,4 +25,4 @@ function RequestStatusModalBg(props) {
   );
 }
 
-export default RequestStatusModalBg;
+export default connect(null, { resetAppState })(RequestStatusModalBg);
