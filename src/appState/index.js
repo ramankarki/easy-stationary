2a69ca1e751cd73e793a6b0a3f2ef8bf1436c254
Raman Karki <ramankarki40@gmail.com>
2021-08-29 10:15:37 +0545

    add user app state

--- src/appState/index.js
+++ src/appState/index.js
@@ -1,9 +1,11 @@
 import auth from './auth';
+import user from './user';
 import category from './category';
 
 const appState = {
   ...auth,
   ...category,
+  ...user,
 };
 
 const getAppState = (TYPE) => ({
