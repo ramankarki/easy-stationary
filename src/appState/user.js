f0008c43eb21a5ad97282f309b94f484b51163dc
Raman Karki <ramankarki40@gmail.com>
2021-08-30 23:19:51 +0545

    add APP_USER_EMAIL_UPDATE_STATE, APP_USER_PASSWORD_UPDATE_STATE

--- src/appState/user.js
+++ src/appState/user.js
@@ -1,20 +1,69 @@
-import { USER, APP_USER_STATE } from '../actions/constants';
+import {
+  USER,
+  APP_USER_STATE,
+  APP_USER_EMAIL_UPDATE_STATE,
+  APP_USER_PASSWORD_UPDATE_STATE,
+} from '../actions/constants';
 import getErrorTag from '../utils/errorTags';
+import { ADMIN_SETTINGS } from '../Routes/contants';
+
+import LazyImg from '../components/LazyImg';
 
 const user = {
   [APP_USER_STATE]: {
     getRoute: () => `/api/v1/user/me`,
     modalMsg: (requestStatus, errorTag) => {
       switch (requestStatus) {
-        case 'patchSuccess':
-          return;
-        case 'deleteSuccess':
-          return;
         default:
           return getErrorTag(errorTag);
       }
     },
     domainState: USER,
+  },
+  [APP_USER_EMAIL_UPDATE_STATE]: {
+    postRoute: () =>
+      `/api/v1/user/req-update-email?user-settings-page=${window.location.origin}/%23${ADMIN_SETTINGS}`,
+    noReset: true,
+    modalMsg: (requestStatus, errorTag) => {
+      switch (requestStatus) {
+        case 'postSuccess':
+          return (
+            <div className="modalBg__modalMsg">
+              <picture>
+                <LazyImg src="/assets/check.svg" alt="success icon" />
+              </picture>
+              <p>
+                Email has been sent! Please{' '}
+                <a href="https://gmail.com" target="_blank" rel="noreferrer">
+                  confirm
+                </a>{' '}
+                your email to update. (Do check your promotions tab if you don't
+                see any email at inbox)
+              </p>
+            </div>
+          );
+        default:
+          return getErrorTag(errorTag);
+      }
+    },
+  },
+  [APP_USER_PASSWORD_UPDATE_STATE]: {
+    patchRoute: () => `/api/v1/user/update-password`,
+    modalMsg: (requestStatus, errorTag) => {
+      switch (requestStatus) {
+        case 'patchtSuccess':
+          return (
+            <div className="modalBg__modalMsg">
+              <picture>
+                <LazyImg src="/assets/check.svg" alt="success icon" />
+              </picture>
+              <p>Password changed successfuly!</p>
+            </div>
+          );
+        default:
+          return getErrorTag(errorTag);
+      }
+    },
   },
 };
 
