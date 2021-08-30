4b317c44bb8650a52469a135a91f7c5bb5bdc322
Raman Karki <ramankarki40@gmail.com>
2021-08-30 15:56:41 +0545

    add red background for danger variant of button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,7 +3,10 @@ import classes from '../../utils/classes';
 import './button.scss';
 
 function Button(props) {
-  const classNames = classes('button', { 'button-small': props.small });
+  const classNames = classes('button', {
+    'button-small': props.small,
+    'button-danger': props.danger,
+  });
   return (
     <button {...props} className={classNames}>
       {props.value}
