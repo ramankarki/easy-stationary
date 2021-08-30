a0f1d6cba2b347fb48506495cf39e0d5c42da190
Raman Karki <ramankarki40@gmail.com>
2021-08-30 15:58:56 +0545

    action creator for delete request

---
+++ src/actions/onDelete.js
@@ -0,0 +1,46 @@
+import { batch } from 'react-redux';
+
+import API from '../utils/API';
+import { DELETE, CRITICAL_MODAL_STATE } from './constants';
+import { ejectReducer } from '../utils/dynamicReducers';
+
+const onDelete =
+  (APP_STATE, deleteObj, ...args) =>
+  (dispatch, getState) => {
+    const appState = getState()[APP_STATE];
+    const { domainState, dynamicState } = appState;
+
+    dispatch({
+      type: APP_STATE,
+      payload: { ...appState, requestStatus: 'pending' },
+    });
+
+    console.log(args, 'bmxcvbnmvnbvcnnbvnmnbvcvbnbcv7ertuytriokdfrfd');
+
+    API.delete(appState.deleteRoute(...args))
+      .then(() =>
+        batch(() => {
+          ejectReducer(CRITICAL_MODAL_STATE);
+          dispatch({
+            type: dynamicState ? domainState + DELETE : domainState,
+            payload: deleteObj,
+          });
+          dispatch({
+            type: APP_STATE,
+            payload: { ...appState, requestStatus: null },
+          });
+        })
+      )
+      .catch(({ response }) =>
+        dispatch({
+          type: APP_STATE,
+          payload: {
+            ...appState,
+            errorTag: response.data.message.split(':')[0],
+            requestStatus: 'failed',
+          },
+        })
+      );
+  };
+
+export default onDelete;
