09d1eda6a8d3725effb3168f13edbc0ac1974c7c
Raman Karki <ramankarki40@gmail.com>
2021-08-31 16:41:09 +0545

    add extra args on postRoute func to get post route

--- src/actions/onPost.js
+++ src/actions/onPost.js
@@ -3,94 +3,96 @@ import { batch } from 'react-redux';
 import API from '../utils/API';
 import { CREATE } from './constants';
 
-const onPost = (APP_STATE_TYPE, UI_STATE_TYPE) => (dispatch, getState) => {
-  const uiState = getState()[UI_STATE_TYPE];
-  const appState = getState()[APP_STATE_TYPE];
-  const { domainState, dynamicState, noSuccessModal, noReset } = appState;
+const onPost =
+  (APP_STATE_TYPE, UI_STATE_TYPE, ...args) =>
+  (dispatch, getState) => {
+    const uiState = getState()[UI_STATE_TYPE];
+    const appState = getState()[APP_STATE_TYPE];
+    const { domainState, dynamicState, noSuccessModal, noReset } = appState;
 
-  const password = getState()[UI_STATE_TYPE]['Password']?.value;
+    const password = getState()[UI_STATE_TYPE]['Password']?.value;
 
-  let isSubmitable = true;
+    let isSubmitable = true;
 
-  // to check if validation fails onSubmit
-  const newUIState = {};
+    // to check if validation fails onSubmit
+    const newUIState = {};
 
-  // to reset UI state onSubmit success
-  const resetUIState = {};
+    // to reset UI state onSubmit success
+    const resetUIState = {};
 
-  // to submit on API if form isSubmitable
-  const apiData = {};
+    // to submit on API if form isSubmitable
+    const apiData = {};
 
-  // validate before submitting & collect data in new obj for API call
-  for (let fieldName in uiState) {
-    resetUIState[fieldName] = { ...uiState[fieldName], value: '' };
+    // validate before submitting & collect data in new obj for API call
+    for (let fieldName in uiState) {
+      resetUIState[fieldName] = { ...uiState[fieldName], value: '' };
 
-    let { value, dbProp } = uiState[fieldName];
-    const checkValidation = !uiState[fieldName].validate(value, password);
+      let { value, dbProp } = uiState[fieldName];
+      const checkValidation = !uiState[fieldName].validate(value, password);
 
-    newUIState[fieldName] = {
-      ...uiState[fieldName],
-      validationFailed: checkValidation,
-    };
-    apiData[dbProp] = value;
+      newUIState[fieldName] = {
+        ...uiState[fieldName],
+        validationFailed: checkValidation,
+      };
+      apiData[dbProp] = value;
 
-    if (isSubmitable && checkValidation) isSubmitable = false;
-  }
+      if (isSubmitable && checkValidation) isSubmitable = false;
+    }
 
-  if (isSubmitable) {
-    // start ajax req
-    dispatch({
-      type: APP_STATE_TYPE,
-      payload: { ...appState, requestStatus: appState.requestEnum.pending },
-    });
+    if (isSubmitable) {
+      // start ajax req
+      dispatch({
+        type: APP_STATE_TYPE,
+        payload: { ...appState, requestStatus: appState.requestEnum.pending },
+      });
 
-    API.post(appState.postRoute(), apiData)
-      .then(({ data }) => {
-        // save user data in localStorage
-        if (appState.domainState === 'USER') {
-          // add 90 days
-          data.expiryDate = Date.now() + 7776000000 + '';
-          localStorage.setItem('USER', JSON.stringify(data));
-        }
+      API.post(appState.postRoute(...args), apiData)
+        .then(({ data }) => {
+          // save user data in localStorage
+          if (appState.domainState === 'USER') {
+            // add 90 days
+            data.expiryDate = Date.now() + 7776000000 + '';
+            localStorage.setItem('USER', JSON.stringify(data));
+          }
 
-        batch(() => {
-          // dispatch USER domain data
-          dispatch({
-            type: dynamicState ? domainState + CREATE : domainState || '',
-            payload: data,
+          batch(() => {
+            // dispatch USER domain data
+            dispatch({
+              type: dynamicState ? domainState + CREATE : domainState || '',
+              payload: data,
+            });
+
+            dispatch({
+              type: APP_STATE_TYPE,
+              payload: {
+                ...appState,
+                requestStatus: noSuccessModal
+                  ? null
+                  : appState.requestEnum.postSuccess,
+              },
+            });
+
+            // reset UI state
+            if (!noReset)
+              dispatch({
+                type: UI_STATE_TYPE,
+                payload: resetUIState,
+              });
           });
-
+        })
+        .catch(({ response }) => {
           dispatch({
             type: APP_STATE_TYPE,
             payload: {
               ...appState,
-              requestStatus: noSuccessModal
-                ? null
-                : appState.requestEnum.postSuccess,
+              errorTag: response.data.message.split(':')[0],
+              requestStatus: appState.requestEnum.failed,
             },
           });
-
-          // reset UI state
-          if (!noReset)
-            dispatch({
-              type: UI_STATE_TYPE,
-              payload: resetUIState,
-            });
         });
-      })
-      .catch(({ response }) => {
-        dispatch({
-          type: APP_STATE_TYPE,
-          payload: {
-            ...appState,
-            errorTag: response.data.message.split(':')[0],
-            requestStatus: appState.requestEnum.failed,
-          },
-        });
-      });
-  } else {
-    dispatch({ type: UI_STATE_TYPE, payload: newUIState });
-  }
-};
+    } else {
+      dispatch({ type: UI_STATE_TYPE, payload: newUIState });
+    }
+  };
 
 export default onPost;
