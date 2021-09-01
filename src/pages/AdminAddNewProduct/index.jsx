67197c36604f5a8158b9d2eaa930cd3b60bd7177
raman <ramankari40@gmail.com>
2021-09-01 16:31:41 +0545

    use useEffect to inject reducers and add map common fields

--- src/pages/AdminAddNewProduct/index.jsx
+++ src/pages/AdminAddNewProduct/index.jsx
@@ -1,4 +1,5 @@
 import { connect } from 'react-redux';
+import { useEffect } from 'react';
 
 import {
   UI_SINGLE_PRODUCT_STATE,
@@ -17,35 +18,56 @@ import onChangeAndBlur from '../../actions/onChangeAndBlur';
 
 import AdminPageTemplate from '../../templates/AdminPageTemplate';
 import Button from '../../components/Button';
+import InputField from '../../components/InputField';
 
 import './adminAddNewProduct.scss';
 
 function AdminAddNewProduct(props) {
   const { categories } = props;
   const imageFields = fields('First image', 'Second image', 'Third image');
+  const commonInputFields = fields(
+    'Product name',
+    'Brand name',
+    'Quantity',
+    'Price (Rs.)'
+  );
+  const unCommonFields = fields(
+    'Category name',
+    'Product description',
+    'Product qualities',
+    'Product specification'
+  );
 
   // inject category
-  injectReducer(
-    APP_CATEGORY_STATE,
-    HOFreducer(APP_CATEGORY_STATE, appState(APP_CATEGORY_STATE))
-  );
-  injectReducer(
-    CATEGORY,
-    HOFdomainReducer(CATEGORY, 'categories', 'category', 'categoryName')
-  );
+  useEffect(() => {
+    injectReducer(
+      APP_CATEGORY_STATE,
+      HOFreducer(APP_CATEGORY_STATE, appState(APP_CATEGORY_STATE))
+    );
+    injectReducer(
+      CATEGORY,
+      HOFdomainReducer(CATEGORY, 'categories', 'category', 'categoryName')
+    );
 
-  if (!categories) props.onGet(APP_CATEGORY_STATE);
+    if (!categories) props.onGet(APP_CATEGORY_STATE);
+  }, []);
 
   // inject single product
-  injectReducer(SINGLE_PRODUCT, HOFreducer(SINGLE_PRODUCT, {}));
-  injectReducer(
-    APP_SINGLE_PRODUCT_STATE,
-    HOFreducer(APP_SINGLE_PRODUCT_STATE, appState(APP_SINGLE_PRODUCT_STATE))
-  );
-  injectReducer(
-    UI_SINGLE_PRODUCT_STATE,
-    HOFreducer(UI_SINGLE_PRODUCT_STATE, { ...imageFields })
-  );
+  useEffect(() => {
+    injectReducer(SINGLE_PRODUCT, HOFreducer(SINGLE_PRODUCT, {}));
+    injectReducer(
+      APP_SINGLE_PRODUCT_STATE,
+      HOFreducer(APP_SINGLE_PRODUCT_STATE, appState(APP_SINGLE_PRODUCT_STATE))
+    );
+    injectReducer(
+      UI_SINGLE_PRODUCT_STATE,
+      HOFreducer(UI_SINGLE_PRODUCT_STATE, {
+        ...imageFields,
+        ...commonInputFields,
+        ...unCommonFields,
+      })
+    );
+  }, []);
 
   const onSelectImage = (fieldName) => (event) => {
     props.onChangeAndBlur(
@@ -70,6 +92,7 @@ function AdminAddNewProduct(props) {
       APP_STATE={APP_SINGLE_PRODUCT_STATE}
     >
       <form onSubmit={onSumitHandler} className="product">
+        {/* input type file for selecting images */}
         {Object.keys(imageFields).map((fieldName) => (
           <div key={fieldName} className="product__image">
             <label className="product__image">
@@ -96,6 +119,16 @@ function AdminAddNewProduct(props) {
             )}
           </div>
         ))}
+
+        {/* common input fields */}
+        {Object.keys(commonInputFields).map((fieldName) => (
+          <InputField
+            key={fieldName}
+            TYPE={UI_SINGLE_PRODUCT_STATE}
+            labelName={fieldName}
+            dbProp={commonInputFields[fieldName].dbProp}
+          />
+        ))}
       </form>
     </AdminPageTemplate>
   );
@@ -116,50 +149,3 @@ const mapStateToProps = ({
 export default connect(mapStateToProps, { onChangeAndBlur, onGet })(
   AdminAddNewProduct
 );
-
-//   imageUrl: [],
-//   productName: {
-//     type: String,
-//     trim: true,
-//     required: [true, 'productName is required!'],
-//   },
-//   productId: {
-//     type: String,
-//     required: [true, 'productId is required!'],
-//   },
-//   categoryName: {
-//     type: String,
-//     trim: true,
-//     required: [true, 'categoryName is required!'],
-//   },
-//   brandName: {
-//     type: String,
-//     trim: true,
-//     required: [true, 'brandName is required!'],
-//   },
-//   price: {
-//     type: Number,
-//     required: [true, 'price is required!'],
-//   },
-//   quantity: {
-//     type: Number,
-//     required: [true, 'quantity is required!'],
-//   },
-//   productDescription: {
-//     type: String,
-//     trim: true,
-//     required: [true, 'productDescription is required!'],
-//   },
-//   productQualities: [String],
-//   productSpecification: Object,
-//   ratings: {
-//     1: { type: Number, default: 0 },
-//     2: { type: Number, default: 0 },
-//     3: { type: Number, default: 0 },
-//     4: { type: Number, default: 0 },
-//     5: { type: Number, default: 0 },
-//   },
-//   noOfPurchases: {
-//     type: Number,
-//     default: 0,
-//   },
