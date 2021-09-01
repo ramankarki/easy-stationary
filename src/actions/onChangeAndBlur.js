f84f293469f9c235f5417f93ee68eb1b992cc55b
Raman Karki <ramankarki40@gmail.com>
2021-09-01 07:56:51 +0545

    store image after reading and converting into base64 if field type is image

--- src/actions/onChangeAndBlur.js
+++ src/actions/onChangeAndBlur.js
@@ -25,6 +25,24 @@ const onChangeAndBlur =
         [field]: { ...state[field], value, validationFailed },
       },
     });
+
+    if (field.includes('image')) {
+      const reader = new FileReader();
+      reader.readAsDataURL(value);
+      reader.onload = (event) =>
+        dispatch({
+          type: TYPE,
+          payload: {
+            ...state,
+            [field]: {
+              ...state[field],
+              value,
+              validationFailed,
+              base64: event.target.result,
+            },
+          },
+        });
+    }
   };
 
 export default onChangeAndBlur;
