9e3c95401efb52fe45d4dc9b7d3f075f934e4dc5
raman <ramankari40@gmail.com>
2021-09-02 20:10:43 +0545

    import Header, BreadCrumb - inject APP_SINGLE_PRODUCT_STATE, SINGLE_PRODUCT - call onGet action creator to get single product data - use connect func to get products data from state

--- src/pages/SingleProduct/index.jsx
+++ src/pages/SingleProduct/index.jsx
@@ -1,7 +1,43 @@
+import { connect } from 'react-redux';
+
+import { injectReducer } from '../../utils/dynamicReducers';
+import {
+  APP_SINGLE_PRODUCT_STATE,
+  SINGLE_PRODUCT,
+} from '../../actions/constants';
+import HOFreducer from '../../reducers/HOFreducer';
+import appState from '../../appState';
+import onGet from '../../actions/onGet';
+
+import Header from '../../templates/Header';
+import BreadCrumb from '../../components/BreadCrumb';
+
 import './singleProduct.scss';
 
 function SingleProduct(props) {
-  return <h1>single product page</h1>;
+  const { product } = props;
+
+  injectReducer(SINGLE_PRODUCT, HOFreducer(SINGLE_PRODUCT, {}));
+  injectReducer(
+    APP_SINGLE_PRODUCT_STATE,
+    HOFreducer(APP_SINGLE_PRODUCT_STATE, appState(APP_SINGLE_PRODUCT_STATE))
+  );
+
+  if (!product) props.onGet(APP_SINGLE_PRODUCT_STATE);
+
+  return (
+    <div className="singleProduct">
+      <Header />
+      <BreadCrumb />
+
+      <div className="singleProduct__heroSection">
+        <picture></picture>
+        <div className="singleProduct__heroSection__content"></div>
+      </div>
+    </div>
+  );
 }
 
-export default SingleProduct;
+const mapStateToProps = ({ SINGLE_PRODUCT }) => ({ ...SINGLE_PRODUCT });
+
+export default connect(mapStateToProps, { onGet })(SingleProduct);
