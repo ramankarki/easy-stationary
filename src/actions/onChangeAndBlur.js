8a752438ebabeb2fb062e8bca0463a91b224ec22
raman <ramankari40@gmail.com>
2021-09-02 13:27:39 +0545

    update image to cloudinary when admin selects an image

--- src/actions/onChangeAndBlur.js
+++ src/actions/onChangeAndBlur.js
@@ -29,9 +29,12 @@ const onChangeAndBlur =
     });
 
     if (field.includes('image')) {
+      let base64;
       if (value) {
+        // read and dispatch in client side
         reader.readAsDataURL(value);
-        reader.onload = (event) =>
+        reader.onload = (event) => {
+          base64 = event.target.result;
           dispatch({
             type: TYPE,
             payload: {
@@ -43,6 +46,35 @@ const onChangeAndBlur =
                 base64: event.target.result,
               },
             },
+          });
+        };
+
+        // upload image in cloudinary
+        const formData = new FormData();
+        formData.append('file', value);
+        formData.append('upload_preset', process.env.REACT_APP_UPLOAD_PRESIST);
+        fetch(
+          `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload`,
+          {
+            method: 'POST',
+            body: formData,
+          }
+        )
+          .then((res) => res.json())
+          .then(({ secure_url }) => {
+            dispatch({
+              type: TYPE,
+              payload: {
+                ...state,
+                [field]: {
+                  ...state[field],
+                  value,
+                  validationFailed,
+                  base64,
+                  src: secure_url,
+                },
+              },
+            });
           });
       } else {
         dispatch({
