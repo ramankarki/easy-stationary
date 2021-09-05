738d6042b72c44b6d6d601bbe31c3d63dff9515f
raman <ramankari40@gmail.com>
2021-09-05 21:56:34 +0545

    return rating stars images inside div by calculating avg ratings from ratings obj

---
+++ src/components/RatingStars/index.jsx
@@ -0,0 +1,27 @@
+import inActive from './inactive-star.svg';
+import active from './active-star.svg';
+import './ratingStars.scss';
+
+function RatingsStar(props) {
+  if (!props.ratings) return '';
+
+  let noOfPeople = 0,
+    totalRatings = 0;
+  for (let star in props.ratings) {
+    noOfPeople += props.ratings[star];
+    totalRatings += props.ratings[star] * +star;
+  }
+  const avg = Math.round(totalRatings / noOfPeople);
+
+  return (
+    <div className="ratings">
+      {Object.keys(props.ratings).map((star, index) => {
+        let src = inActive;
+        if (index + 1 <= avg) src = active;
+        return <img src={src} alt="star" />;
+      })}
+    </div>
+  );
+}
+
+export default RatingsStar;
