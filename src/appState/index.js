bc13edb55ba4d8e41a385d0bcbddb56b7bc09c7c
raman <ramankari40@gmail.com>
2021-09-06 18:59:01 +0545

    import singleCategoryProducts

--- src/appState/index.js
+++ src/appState/index.js
@@ -2,12 +2,14 @@ import auth from './auth';
 import user from './user';
 import category from './category';
 import singleProduct from './singleProduct';
+import singleCategoryProducts from './singleCategoryProducts';
 
 const appState = {
   ...auth,
   ...category,
   ...user,
   ...singleProduct,
+  ...singleCategoryProducts,
 };
 
 const getAppState = (TYPE) => ({
