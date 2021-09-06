f54f89a90352a78be677aa04b4a1306fe6acdf3e
raman <ramankari40@gmail.com>
2021-09-06 16:00:03 +0545

    add args params and pass in getRoute func

--- src/actions/onGet.js
+++ src/actions/onGet.js
@@ -3,44 +3,46 @@ import { batch } from 'react-redux';
 import API from '../utils/API';
 import { READ } from './constants';
 
-const onGet = (APP_STATE) => (dispatch, getState) => {
-  const appState = getState()[APP_STATE];
-  const { domainState, dynamicState } = appState;
+const onGet =
+  (APP_STATE, ...args) =>
+  (dispatch, getState) => {
+    const appState = getState()[APP_STATE];
+    const { domainState, dynamicState } = appState;
 
-  dispatch({
-    type: APP_STATE,
-    payload: { ...appState, requestStatus: 'pending' },
-  });
+    dispatch({
+      type: APP_STATE,
+      payload: { ...appState, requestStatus: 'pending' },
+    });
 
-  API.get(appState.getRoute())
-    .then(({ data }) =>
-      batch(() => {
-        if (appState.domainState === 'USER') {
-          const user = JSON.parse(localStorage.getItem('USER'));
-          user.user = data.user;
-          localStorage.setItem('USER', JSON.stringify(user));
-        }
+    API.get(appState.getRoute(...args))
+      .then(({ data }) =>
+        batch(() => {
+          if (appState.domainState === 'USER') {
+            const user = JSON.parse(localStorage.getItem('USER'));
+            user.user = data.user;
+            localStorage.setItem('USER', JSON.stringify(user));
+          }
 
-        dispatch({
-          type: dynamicState ? domainState + READ : domainState,
-          payload: data,
-        });
+          dispatch({
+            type: dynamicState ? domainState + READ : domainState,
+            payload: data,
+          });
+          dispatch({
+            type: APP_STATE,
+            payload: { ...appState, requestStatus: null },
+          });
+        })
+      )
+      .catch(({ response }) =>
         dispatch({
           type: APP_STATE,
-          payload: { ...appState, requestStatus: null },
-        });
-      })
-    )
-    .catch(({ response }) =>
-      dispatch({
-        type: APP_STATE,
-        payload: {
-          ...appState,
-          errorTag: response.data.message.split(':')[0],
-          requestStatus: 'failed',
-        },
-      })
-    );
-};
+          payload: {
+            ...appState,
+            errorTag: response.data.message.split(':')[0],
+            requestStatus: 'failed',
+          },
+        })
+      );
+  };
 
 export default onGet;
