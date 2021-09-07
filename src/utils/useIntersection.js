78f9b72ac297a9e26ea92f590db6af4556b745ca
raman <ramankari40@gmail.com>
2021-09-07 07:54:40 +0545

    IntersectionObserver API setup

---
+++ src/utils/useIntersection.js
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+
+let callBackFunc;
+
+function handleIntersections(entries, observer) {
+  entries.forEach((entry) => {
+    // if element is near viewport, call func
+    if (entry.isIntersecting) callBackFunc();
+  });
+}
+
+export function useIntersection(elem, callback) {
+  // change scope to call later
+  callBackFunc = callback;
+
+  useEffect(() => {
+    const target = elem.current;
+    let observer = new IntersectionObserver(handleIntersections, {
+      rootMargin: '350px',
+      threshold: '0',
+    });
+    observer.observe(target);
+
+    return () => {
+      observer.unobserve(target);
+    };
+  });
+}
