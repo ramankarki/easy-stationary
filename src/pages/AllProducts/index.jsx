adbb8cbef69e4c108dc3dd99f054ebfbd2e46d91
raman <ramankari40@gmail.com>
2021-09-07 21:33:42 +0545

    responsive category bar setup

--- src/pages/AllProducts/index.jsx
+++ src/pages/AllProducts/index.jsx
@@ -1,15 +1,47 @@
+import { connect } from 'react-redux';
+import { useEffect } from 'react';
+
+import onGet from '../../actions/onGet';
+import { APP_CATEGORY_STATE, CATEGORY } from '../../actions/constants';
+import { injectReducer } from '../../utils/dynamicReducers';
+import HOFreducer from '../../reducers/HOFreducer';
+import HOFdomainReducer from '../../reducers/HOFdomainReducer';
+import appState from '../../appState';
+import { ROOT } from '../../Routes/contants';
+
 import Header from '../../templates/Header';
 import SpinnerLoading from '../../components/SpinnerLoading';
 import BreadCrumb from '../../components/BreadCrumb';
+import LinkButton from '../../components/LinkButton';
 
 import './allProducts.scss';
 
-function AllProducts() {
+function AllProducts(props) {
+  let { categories } = props;
+  const noOfProducts = categories?.reduce(
+    (acc, val) => acc + val.noOfProducts,
+    0
+  );
+  categories?.unshift({ noOfProducts, categoryName: 'All products' });
+
+  injectReducer(CATEGORY, HOFdomainReducer(CATEGORY, 'categories', 'category'));
+  injectReducer(
+    APP_CATEGORY_STATE,
+    HOFreducer(APP_CATEGORY_STATE, appState(APP_CATEGORY_STATE))
+  );
+
+  useEffect(() => {
+    props.onGet(APP_CATEGORY_STATE);
+  }, []);
+
+  const path = window.location.hash.split('/')[1] || 'All products';
+
   return (
     <div className="allProducts">
       <Header />
       <BreadCrumb />
 
+      {/* hero section */}
       <div className="allProducts__heroSection">
         <picture
           style={{ backgroundImage: `url('/assets/hero image.webp')` }}
@@ -19,8 +51,24 @@ function AllProducts() {
           <p>
             Search for the products you want by simplying using search box above
             and typing keywords, or by clicking the category buttons of your
-            products.
+            products below.
           </p>
+        </div>
+      </div>
+
+      {/* category bar */}
+      <div className="allProducts__categoryBar">
+        <div className="allProducts__categoryBarWrapper">
+          {categories?.map(({ noOfProducts, categoryName }, index) => (
+            <LinkButton
+              key={categoryName}
+              to={!index ? ROOT : '/' + categoryName}
+              dark={path === categoryName}
+            >
+              {categoryName}
+              <span>({noOfProducts})</span>
+            </LinkButton>
+          ))}
         </div>
       </div>
 
@@ -32,4 +80,6 @@ function AllProducts() {
   );
 }
 
-export default AllProducts;
+const mapStateToProps = ({ CATEGORY }) => ({ ...CATEGORY });
+
+export default connect(mapStateToProps, { onGet })(AllProducts);
