e9d365076dfccd2812eb2f93d463e20f911a57fb
raman <ramankari40@gmail.com>
2021-09-08 17:21:19 +0545

    return same array if sortProp is undefined

--- src/utils/sortArray.js
+++ src/utils/sortArray.js
@@ -1,11 +1,14 @@
 const sortArray = (array, sortProp) => {
   const copyArray = [...array];
 
-  if (sortProp)
-    return copyArray.sort((a, b) =>
-      a[sortProp] < b[sortProp] ? -1 : a[sortProp] > b[sortProp] ? 1 : 0
-    );
-  return copyArray.sort((a, b) => (a < b ? -1 : a > b ? 1 : 0));
+  if (!sortProp) return copyArray;
+
+  if (sortProp === 'asc')
+    return copyArray.sort((a, b) => (a < b ? -1 : a > b ? 1 : 0));
+
+  return copyArray.sort((a, b) =>
+    a[sortProp] < b[sortProp] ? -1 : a[sortProp] > b[sortProp] ? 1 : 0
+  );
 };
 
 export default sortArray;
