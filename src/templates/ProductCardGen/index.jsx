9a686b5a63d0b1b3c80801e13ad414ee54896de5
raman <ramankari40@gmail.com>
2021-09-08 16:42:06 +0545

    transforme imageUrl first image ratio to 1:1

--- src/templates/ProductCardGen/index.jsx
+++ src/templates/ProductCardGen/index.jsx
@@ -7,22 +7,28 @@ import './productCardGen.scss';
 function ProductCardGen(props) {
   return (
     <div className="multipleProducts">
-      {props.products?.map((product) => (
-        <Link
-          to={`/${product.categoryName}/${product.productId}`}
-          style={{ textDecoration: 'none' }}
-          key={product.productId}
-        >
-          <span className="multipleProducts__product">
-            <picture>
-              <img src={product.imageUrl[0]} alt="product" />
-            </picture>
-            <RatingStars ratings={product.ratings} peopleNumber="true" />
-            <h3>{product.productName}</h3>
-            <p>Rs. {product.price}</p>
-          </span>
-        </Link>
-      ))}
+      {props.products?.map((product) => {
+        const breakUrl = product.imageUrl[0].split('upload');
+        product.imageUrl[0] =
+          breakUrl[0] + 'upload/ar_3:2,c_fill' + breakUrl[1];
+
+        return (
+          <Link
+            to={`/${product.categoryName}/${product.productId}`}
+            style={{ textDecoration: 'none' }}
+            key={product.productId}
+          >
+            <span className="multipleProducts__product">
+              <picture>
+                <img src={product.imageUrl[0]} alt="product" />
+              </picture>
+              <RatingStars ratings={product.ratings} peopleNumber="true" />
+              <h3>{product.productName}</h3>
+              <p>Rs. {product.price}</p>
+            </span>
+          </Link>
+        );
+      })}
     </div>
   );
 }
