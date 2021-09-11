269ec5ecd1c3a558900ddb9f74a15a54c66b18d3
raman <ramankari40@gmail.com>
2021-09-11 20:00:26 +0545

    get APP_SHOPPING_CART_STATE, SHOPPING_CART from store using connect, setup RequestStatusModalBg

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,6 +1,12 @@
+import { connect } from 'react-redux';
+
+import { APP_SHOPPING_CART_STATE } from '../../actions/constants';
+
 import Header from '../../templates/Header';
 import BreadCrumb from '../../components/BreadCrumb';
 import HeroSection from '../../templates/HeroSection';
+import RequestStatusModalBg from '../../templates/RequestStatusModalBg';
+import SpinnerLoading from '../../components/SpinnerLoading';
 
 import './cart.scss';
 
@@ -14,8 +20,27 @@ function Cart(props) {
         heading="Place your order"
         text='Select the product quantity you want. Place your order by clicking "Place order" button and your products will arive at your address in no time.'
       />
+
+      {/* modal */}
+      {props.requestStatus && (
+        <RequestStatusModalBg
+          requestStatus={props.requestStatus}
+          APP_STATE={APP_SHOPPING_CART_STATE}
+        >
+          {props.requestStatus === 'pending' ? (
+            <SpinnerLoading />
+          ) : (
+            props.modalMsg(props.requestStatus, props.errorTag)
+          )}
+        </RequestStatusModalBg>
+      )}
     </div>
   );
 }
 
-export default Cart;
+const mapStateToProps = ({ APP_SHOPPING_CART_STATE, SHOPPING_CART }) => ({
+  ...APP_SHOPPING_CART_STATE,
+  ...SHOPPING_CART,
+});
+
+export default connect(mapStateToProps, {})(Cart);
