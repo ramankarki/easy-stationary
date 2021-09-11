5513d1139bbbdb7ed69b391a9a44bf7761f2ded3
raman <ramankari40@gmail.com>
2021-09-11 19:46:09 +0545

    import Header, BreadCrumb ahd HeroSection

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,5 +1,21 @@
+import Header from '../../templates/Header';
+import BreadCrumb from '../../components/BreadCrumb';
+import HeroSection from '../../templates/HeroSection';
+
+import './cart.scss';
+
 function Cart(props) {
-  return <h1>Cart page</h1>;
+  return (
+    <div className="cart">
+      <Header />
+      <BreadCrumb />
+      <HeroSection
+        imgUrl="url('/assets/cart.webp')"
+        heading="Place your order"
+        text='Select the product quantity you want. Place your order by clicking "Place order" button and your products will arive at your address in no time.'
+      />
+    </div>
+  );
 }
 
 export default Cart;
