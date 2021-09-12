edd1fadf437df12a83c0bbbff6fb0cd554196c8f
raman <ramankari40@gmail.com>
2021-09-12 16:57:03 +0545

    show no proucts msg if shopping cart is empty

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import {
   APP_SHOPPING_CART_STATE,
@@ -10,6 +11,7 @@ import HOFreducer from '../../reducers/HOFreducer';
 import fields from '../../utils/fields';
 import onChangeAndBlur from '../../actions/onChangeAndBlur';
 import onDelete from '../../actions/onDelete';
+import { ROOT } from '../../Routes/contants';
 
 import Header from '../../templates/Header';
 import BreadCrumb from '../../components/BreadCrumb';
@@ -54,84 +56,94 @@ function Cart(props) {
         text='Select the product quantity you want. Place your order by clicking "Place order" button and your products will arive at your address in no time. Payment will be on cash after delivery.'
       />
 
-      <div className="cart__table">
-        <div className="cart__productsTable">
-          <div className="cart__tableHead">
-            <p>Product</p>
-            <p>Quantity</p>
-            <p>Price</p>
-          </div>
-          {props.OrderProducts?.value?.map(
-            (
-              {
-                categoryName,
-                productId,
-                imageUrl,
-                price,
-                quantity,
-                productName,
-              },
-              index
-            ) => (
-              <div key={productId} className="cart__tableCell tableCell">
-                <div className="tableCell__product">
-                  <img src={imageUrl[0]} alt="product" />
-                  <p>{productName}</p>
-                </div>
-                <select value={quantity} onChange={onQuantityChange(index)}>
-                  {Array(props.shoppingCart[index]?.quantity)
-                    .fill(0)
-                    .map((val, i) => (
-                      <option key={Date.now() + i + 'option'} value={i + 1}>
-                        {i + 1}
-                      </option>
-                    ))}
-                </select>
-                <p className="tableCell__price">{quantity * +price}</p>
-                <Button
-                  onClick={removeProduct(productId)}
-                  style={{ gridColumn: 'span 1', backgroundColor: 'white' }}
-                >
-                  <img src="/assets/exit icon.svg" alt="exit icon" />
+      {props.shoppingCart &&
+        (props.shoppingCart.length ? (
+          <div className="cart__table">
+            <div className="cart__productsTable">
+              <div className="cart__tableHead">
+                <p>Product</p>
+                <p>Quantity</p>
+                <p>Price</p>
+              </div>
+              {props.OrderProducts?.value?.map(
+                (
+                  {
+                    categoryName,
+                    productId,
+                    imageUrl,
+                    price,
+                    quantity,
+                    productName,
+                  },
+                  index
+                ) => (
+                  <div key={productId} className="cart__tableCell tableCell">
+                    <div className="tableCell__product">
+                      <img src={imageUrl[0]} alt="product" />
+                      <p>{productName}</p>
+                    </div>
+                    <select value={quantity} onChange={onQuantityChange(index)}>
+                      {Array(props.shoppingCart[index]?.quantity)
+                        .fill(0)
+                        .map((val, i) => (
+                          <option key={Date.now() + i + 'option'} value={i + 1}>
+                            {i + 1}
+                          </option>
+                        ))}
+                    </select>
+                    <p className="tableCell__price">{quantity * +price}</p>
+                    <Button
+                      onClick={removeProduct(productId)}
+                      style={{ gridColumn: 'span 1', backgroundColor: 'white' }}
+                    >
+                      <img src="/assets/exit icon.svg" alt="exit icon" />
+                    </Button>
+                  </div>
+                )
+              )}
+            </div>
+            <div className="cart__placeOrder">
+              <h2>Cart</h2>
+              <div>
+                <p>
+                  Total amount:{' '}
+                  <b>
+                    Rs.{' '}
+                    {props.OrderProducts?.value?.reduce(
+                      (acc, { quantity, price }) => acc + quantity * price,
+                      0
+                    )}
+                  </b>
+                </p>
+                <p>
+                  Name:{' '}
+                  <b>
+                    {props.user?.firstName} {props.user?.lastName}
+                  </b>
+                </p>
+                <p>
+                  Phone: <b>+977-{props.user?.phoneNumber}</b>
+                </p>
+                <p>
+                  Shipping address: <b>hello world</b>
+                </p>
+                <Button>
+                  <picture>
+                    <img src="/assets/order icon.svg" alt="cart icon" />
+                  </picture>
+                  Place order
                 </Button>
               </div>
-            )
-          )}
-        </div>
-        <div className="cart__placeOrder">
-          <h2>Cart</h2>
-          <div>
-            <p>
-              Total amount:{' '}
-              <b>
-                Rs.{' '}
-                {props.OrderProducts?.value?.reduce(
-                  (acc, { quantity, price }) => acc + quantity * price,
-                  0
-                )}
-              </b>
-            </p>
-            <p>
-              Name:{' '}
-              <b>
-                {props.user?.firstName} {props.user?.lastName}
-              </b>
-            </p>
-            <p>
-              Phone: <b>+977-{props.user?.phoneNumber}</b>
-            </p>
-            <p>
-              Shipping address: <b>hello world</b>
-            </p>
-            <Button>
-              <picture>
-                <img src="/assets/order icon.svg" alt="cart icon" />
-              </picture>
-              Place order
-            </Button>
+            </div>
           </div>
-        </div>
-      </div>
+        ) : (
+          <p className="noProductsMsg">
+            There are no products on cart.{' '}
+            <Link to={ROOT}>
+              <b>Shop now</b>
+            </Link>
+          </p>
+        ))}
 
       {/* modal */}
       {props.requestStatus && (
