778fda9c3ccc11e9f9866b9984f90a368eb950d6
ramankarki <ramankarki40@gmail.com>
2021-09-13 17:45:15 +0545

    reduce heading and price font size for small screen, add modifer class 'small' to reduce font sizes

--- src/templates/ProductCard/index.jsx
+++ src/templates/ProductCard/index.jsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom';
 
+import classes from '../../utils/classes';
+
 import RatingStars from '../../components/RatingStars';
 
 import './productCard.scss';
 
 function ProductCard(props) {
+  const productCardClass = classes('productCard', {
+    'productCard-small': props.small,
+  });
+
   const blankFunc = () => {};
   return (
     <Link
@@ -12,7 +18,7 @@ function ProductCard(props) {
       to={`/${props.categoryName}/${props.productId}`}
       style={{ textDecoration: 'none' }}
     >
-      <span className="productCard">
+      <span className={productCardClass}>
         <picture>
           <img src={props.imageUrl} alt="product" />
         </picture>
