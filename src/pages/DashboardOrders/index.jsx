5830c9682843303a04e5ea6c8f9be23d156864c5
ramankarki <ramankarki40@gmail.com>
2021-09-13 23:13:57 +0545

    inject BLANK ui state to bypass patch req - cancel order doesn't need body, pass in trigger func to trigger CriticalDeleteModal before calling onOrderCancel func to cancel order

--- src/pages/DashboardOrders/index.jsx
+++ src/pages/DashboardOrders/index.jsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
-import { APP_ORDER_STATE, ORDERS } from '../../actions/constants';
+import {
+  APP_ORDER_STATE,
+  CRITICAL_MODAL_STATE,
+  ORDERS,
+} from '../../actions/constants';
 import appState from '../../appState';
 import HOFreducer from '../../reducers/HOFreducer';
 import HOFdomainReducer from '../../reducers/HOFdomainReducer';
 import { ejectReducer, injectReducer } from '../../utils/dynamicReducers';
 import onGet from '../../actions/onGet';
+import onPatch from '../../actions/onPatch';
+import triggerCriticalModal from '../../utils/triggerCriticalModal';
 
 import ClientDashboard from '../../templates/ClientDashboard';
 import OrderCard from '../../templates/OrderCard';
@@ -25,18 +31,40 @@ function DashboardOrders(props) {
       HOFreducer(APP_ORDER_STATE, appState(APP_ORDER_STATE))
     );
     injectReducer(ORDERS, HOFdomainReducer(ORDERS, 'orders', 'order'));
+    injectReducer(
+      'BLANK',
+      HOFreducer('BLANK', {
+        blank: {
+          value: 'blank',
+          dbProp: 'blank',
+          validate: () => 'blank',
+          validationFailed: 'blank',
+        },
+      })
+    );
 
     props.onGet(APP_ORDER_STATE, () => {}, page, sort, filter);
 
     return () => {
       ejectReducer(APP_ORDER_STATE);
       ejectReducer(ORDERS);
+      ejectReducer('BLANK');
     };
   }, []);
 
   const onStatusFilterChange = (event) => setFilter(event.target.value);
 
   const onOrderIdFilterChange = (event) => setSort(event.target.value);
+
+  const onOrderCancel = (orderObj) => () => {
+    props.onPatch(
+      APP_ORDER_STATE,
+      'BLANK',
+      orderObj,
+      () => ejectReducer(CRITICAL_MODAL_STATE),
+      orderObj.orderId
+    );
+  };
 
   return (
     <ClientDashboard
@@ -66,7 +94,17 @@ function DashboardOrders(props) {
         {/* order cards */}
         <div className="orders__cards">
           {props.orders?.map((order) => (
-            <OrderCard key={order._id} {...order} />
+            <OrderCard
+              key={order._id}
+              {...order}
+              onOrderCancel={triggerCriticalModal(
+                null,
+                'Cancel order',
+                `Type 'cancel', to cancel orderId:${order.orderId}.`,
+                onOrderCancel(order),
+                'Cancel order'
+              )}
+            />
           ))}
         </div>
       </div>
@@ -79,4 +117,4 @@ const mapStateToProps = ({ ORDERS, APP_ORDER_STATE }) => ({
   APP_ORDER_STATE,
 });
 
-export default connect(mapStateToProps, { onGet })(DashboardOrders);
+export default connect(mapStateToProps, { onGet, onPatch })(DashboardOrders);
