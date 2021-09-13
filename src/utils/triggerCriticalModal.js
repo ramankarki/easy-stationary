ad6466760cf2bfc9e9263f01cc9029bc00db15de
ramankarki <ramankarki40@gmail.com>
2021-09-13 22:44:37 +0545

    add one more param - btnValue

--- src/utils/triggerCriticalModal.js
+++ src/utils/triggerCriticalModal.js
@@ -3,13 +3,18 @@ import { CRITICAL_MODAL_STATE } from '../actions/constants';
 import HOFreducer from '../reducers/HOFreducer';
 import fields from './fields';
 
-const triggerCriticalModal = (event, heading, desc, deleteFunc) => () => {
-  const field = fields('Critical modal');
-  field['Critical modal'].heading = heading;
-  field['Critical modal'].desc = desc;
-  field.deleteFunc = deleteFunc;
+const triggerCriticalModal =
+  (event, heading, desc, deleteFunc, btnValue) => () => {
+    const field = fields('Critical modal');
+    field['Critical modal'].heading = heading;
+    field['Critical modal'].desc = desc;
+    field.deleteFunc = deleteFunc;
+    field.btnValue = btnValue || 'Delete';
 
-  injectReducer(CRITICAL_MODAL_STATE, HOFreducer(CRITICAL_MODAL_STATE, field));
-};
+    injectReducer(
+      CRITICAL_MODAL_STATE,
+      HOFreducer(CRITICAL_MODAL_STATE, field)
+    );
+  };
 
 export default triggerCriticalModal;
