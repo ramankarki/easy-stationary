90d213d53a3e190dbeb6532032b6278d08fd8de2
ramankarki <ramankarki40@gmail.com>
2021-09-13 12:24:53 +0545

    rename

--- src/pages/ClientOrders/index.jsx
+++
@@ -1,5 +0,0 @@
-function ClientOrders(props) {
-  return <h1>client orders</h1>;
-}
-
-export default ClientOrders;
---
+++ src/pages/DashboardOrders/index.jsx
@@ -0,0 +1,5 @@
+function DashboardOrders(props) {
+  return <h1>client orders</h1>;
+}
+
+export default DashboardOrders;
