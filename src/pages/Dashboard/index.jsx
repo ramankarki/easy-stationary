cc76a962a4b4ca56a93cedf18898339e05d8c18c
ramankarki <ramankarki40@gmail.com>
2021-09-13 12:22:20 +0545

    dashboard page complete

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,5 +1,54 @@
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { APP_USER_STATE } from '../../actions/constants';
+
+import onGet from '../../actions/onGet';
+
+import ClientDashboard from '../../templates/ClientDashboard';
+
+import './dashboard.scss';
+
 function Dashboard(props) {
-  return <h1>Dashboard page</h1>;
+  useEffect(() => {
+    props.onGet(APP_USER_STATE);
+  }, []);
+
+  return (
+    <ClientDashboard heading="Dashboard" APP_STATE={APP_USER_STATE}>
+      <div className="dashboard">
+        <div className="dashboard__boxes">
+          <p>Total orders</p>
+          <span>
+            {props.user?.pendingOrders +
+              props.user?.deliveredOrders +
+              props.user?.cancelledOrders}
+          </span>
+        </div>
+        <div className="dashboard__boxes">
+          <p>My reviews</p>
+          <span>{props.user?.reviews}</span>
+        </div>
+        <div className="dashboard__boxes">
+          <p>My wishlists</p>
+          <span>0</span>
+        </div>
+        <div className="dashboard__boxes">
+          <p>Pending orders</p>
+          <span>{props.user?.pendingOrders}</span>
+        </div>
+        <div className="dashboard__boxes dashboard__boxes-pending">
+          <p>Delivered orders</p>
+          <span>{props.user?.deliveredOrders}</span>
+        </div>
+        <div className="dashboard__boxes dashboard__boxes-cancelled">
+          <p>Cancelled orders</p>
+          <span>{props.user?.cancelledOrders}</span>
+        </div>
+      </div>
+    </ClientDashboard>
+  );
 }
 
-export default Dashboard;
+const mapStateToProps = ({ USER }) => ({ ...USER });
+
+export default connect(mapStateToProps, { onGet })(Dashboard);
