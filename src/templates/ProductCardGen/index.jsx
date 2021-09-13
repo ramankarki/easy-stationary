6c48dc8e7906e0a6c4fca1f48fd6557bc51607b4
ramankarki <ramankarki40@gmail.com>
2021-09-13 16:39:50 +0545

    separate ProductCard as a template

--- src/templates/ProductCardGen/index.jsx
+++ src/templates/ProductCardGen/index.jsx
@@ -1,35 +1,20 @@
-import { Link } from 'react-router-dom';
-
-import RatingStars from '../../components/RatingStars';
-
+import ProductCard from '../ProductCard';
 import './productCardGen.scss';
 
 function ProductCardGen(props) {
   return (
     <div className="multipleProducts">
       {props.products?.map((product) => {
-        const breakUrl = product.imageUrl[0].split('upload');
-        product.imageUrl[0] =
-          breakUrl[0] + 'upload/ar_3:2,c_fill' + breakUrl[1];
-
-        const blankFunc = () => {};
+        let breakUrl = product.imageUrl[0].split('upload');
+        breakUrl = breakUrl[0] + 'upload/ar_3:2,c_fill' + breakUrl[1];
 
         return (
-          <Link
-            onClick={props.onClick || blankFunc}
-            to={`/${product.categoryName}/${product.productId}`}
-            style={{ textDecoration: 'none' }}
+          <ProductCard
             key={product.productId}
-          >
-            <span className="multipleProducts__product">
-              <picture>
-                <img src={product.imageUrl[0]} alt="product" />
-              </picture>
-              <RatingStars ratings={product.ratings} peopleNumber="true" />
-              <h3>{product.productName}</h3>
-              <p>Rs. {product.price}</p>
-            </span>
-          </Link>
+            {...product}
+            imageUrl={breakUrl}
+            onClick={props.onClick}
+          />
         );
       })}
     </div>
