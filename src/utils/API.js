036f488dde62906232c87d3a18c00af1a0424ac2
ramankarki <ramankarki40@gmail.com>
2021-09-14 16:01:45 +0545

    return axios instance from func to recreate axios instance and update Bearer token

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,13 @@
 import { create } from 'axios';
 
-export default create({
-  baseURL: process.env.REACT_APP_API_1,
-  headers: {
-    authorization: 'Bearer ' + JSON.parse(localStorage.getItem('USER'))?.token,
-  },
-});
+function API() {
+  return create({
+    baseURL: process.env.REACT_APP_API_1,
+    headers: {
+      authorization:
+        'Bearer ' + JSON.parse(localStorage.getItem('USER'))?.token,
+    },
+  });
+}
+
+export default API;
