83b52f85e62172f098c511ff791d4ea713d38e95
ramankarki <ramankarki40@gmail.com>
2021-09-14 10:48:46 +0545

    import ClientDashboard and set heading

---
+++ src/pages/DashboardMyReviews/index.jsx
@@ -0,0 +1,7 @@
+import ClientDashboard from '../../templates/ClientDashboard';
+
+function DashboardMyReviews(props) {
+  return <ClientDashboard heading="My reviews"></ClientDashboard>;
+}
+
+export default DashboardMyReviews;
