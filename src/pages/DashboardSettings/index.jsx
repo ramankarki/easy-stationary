40cfb364383f5b774e81b4120d155f52df66c2a4
ramankarki <ramankarki40@gmail.com>
2021-09-14 10:57:09 +0545

    import ClientDashboard and set heading

---
+++ src/pages/DashboardSettings/index.jsx
@@ -0,0 +1,9 @@
+import ClientDashboard from '../../templates/ClientDashboard';
+
+import './dashboardSettings.scss';
+
+function DashboardSettings(props) {
+  return <ClientDashboard heading="Settings"></ClientDashboard>;
+}
+
+export default DashboardSettings;
