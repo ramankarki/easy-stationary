e4661d1a86c3fc0ac35b214dae5b4c91fe0d4e3c
ramankarki <ramankarki40@gmail.com>
2021-09-20 22:31:32 +0545

    app state for wishlists

---
+++ src/appState/wishlists.js
@@ -0,0 +1,18 @@
+import { APP_WISHLISTS_STATE, WISHLISTS } from '../actions/constants';
+import getErrorTag from '../utils/errorTags';
+
+const wishlists = {
+  [APP_WISHLISTS_STATE]: {
+    getRoute: (categoryName, productId) =>
+      `/api/v1/single-product/${categoryName}/${productId}`,
+    modalMsg: (requestStatus, errorTag) => {
+      switch (requestStatus) {
+        default:
+          return getErrorTag(errorTag);
+      }
+    },
+    domainState: WISHLISTS,
+  },
+};
+
+export default wishlists;
