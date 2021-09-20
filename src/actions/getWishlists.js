c008aeb2c679238ba6fe9126ad86880b621122f6
ramankarki <ramankarki40@gmail.com>
2021-09-20 22:33:55 +0545

    action creator for specific wishlists

---
+++ src/actions/getWishlists.js
@@ -0,0 +1,48 @@
+import { batch } from 'react-redux';
+
+import { APP_WISHLISTS_STATE, WISHLISTS } from './constants';
+import appState from '../appState';
+import API from '../utils/API';
+
+const getWishlists = () => async (dispatch, getState) => {
+  const wishlistsAppState = getState()[APP_WISHLISTS_STATE];
+  const wishlists = JSON.parse(localStorage.getItem('WISHLISTS'));
+
+  const productPromises = Object.keys(wishlists).map((productId) => {
+    return API().get(
+      wishlistsAppState.getRoute(wishlists[productId], productId)
+    );
+  });
+
+  dispatch({
+    type: APP_WISHLISTS_STATE,
+    payload: { ...appState(APP_WISHLISTS_STATE), requestStatus: 'pending' },
+  });
+
+  try {
+    const res = await Promise.all(productPromises);
+    const products = res.map(({ data }) => ({
+      ...data.product,
+      quantity: undefined,
+    }));
+
+    batch(() => {
+      dispatch({
+        type: APP_WISHLISTS_STATE,
+        payload: { ...appState(APP_WISHLISTS_STATE), requestStatus: null },
+      });
+      dispatch({
+        type: WISHLISTS,
+        payload: products,
+      });
+    });
+  } catch ({ response }) {
+    dispatch({
+      type: APP_WISHLISTS_STATE,
+      errorTag: response.data.message.split(':')[0],
+      payload: { ...appState(APP_WISHLISTS_STATE), requestStatus: 'failed' },
+    });
+  }
+};
+
+export default getWishlists;
