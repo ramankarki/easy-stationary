08c5f08bb489a1cbb141934fb34327cfa81c2ee6
ramankarki <ramankarki40@gmail.com>
2021-09-20 22:34:34 +0545

    wishlists page complete

--- src/pages/DashboardMyWishlists/index.jsx
+++ src/pages/DashboardMyWishlists/index.jsx
@@ -1,7 +1,45 @@
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import { injectReducer, ejectReducer } from '../../utils/dynamicReducers';
+import { APP_WISHLISTS_STATE, WISHLISTS } from '../../actions/constants';
+import HOFreducer from '../../reducers/HOFreducer';
+import getWishlists from '../../actions/getWishlists';
+import appState from '../../appState';
+
 import ClientDashboard from '../../templates/ClientDashboard';
+import ProductCardGen from '../../templates/ProductCardGen';
 
 function DashboardMyWishlists(props) {
-  return <ClientDashboard heading="My wishlists"></ClientDashboard>;
+  useEffect(() => {
+    injectReducer(
+      APP_WISHLISTS_STATE,
+      HOFreducer(APP_WISHLISTS_STATE, appState(APP_WISHLISTS_STATE))
+    );
+    injectReducer(WISHLISTS, HOFreducer(WISHLISTS, []));
+
+    props.getWishlists();
+
+    return () => {
+      ejectReducer(APP_WISHLISTS_STATE);
+      ejectReducer(WISHLISTS);
+    };
+  }, []);
+
+  return (
+    <ClientDashboard
+      {...props.APP_WISHLISTS_STATE}
+      heading="My wishlists"
+      APP_STATE={APP_WISHLISTS_STATE}
+    >
+      <ProductCardGen products={props.WISHLISTS} />
+    </ClientDashboard>
+  );
 }
 
-export default DashboardMyWishlists;
+const mapStateToProps = ({ APP_WISHLISTS_STATE, WISHLISTS }) => ({
+  APP_WISHLISTS_STATE,
+  WISHLISTS,
+});
+
+export default connect(mapStateToProps, { getWishlists })(DashboardMyWishlists);
