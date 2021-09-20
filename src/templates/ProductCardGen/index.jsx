b5af7f6b1244a60e8a1af5513b509d9089ca2d02
ramankarki <ramankarki40@gmail.com>
2021-09-20 08:14:01 +0545

    remove wishlists code and pass directly in each product card

--- src/templates/ProductCardGen/index.jsx
+++ src/templates/ProductCardGen/index.jsx
@@ -1,27 +1,7 @@
-import { useState } from 'react';
-
 import ProductCard from '../ProductCard';
 import './productCardGen.scss';
 
 function ProductCardGen(props) {
-  const parseWishlists = JSON.parse(localStorage.getItem('WISHLISTS')) || {};
-  const [wishlists, setWishlists] = useState(parseWishlists);
-
-  const saveWishlists = (productId, categoryName) => (event) => {
-    event.preventDefault();
-    setWishlists((prevState) => {
-      let state = {
-        ...prevState,
-        [productId]: categoryName,
-      };
-
-      if (productId in prevState) delete state[productId];
-
-      localStorage.setItem('WISHLISTS', JSON.stringify(state));
-      return state;
-    });
-  };
-
   return (
     <div className="multipleProducts">
       {props.products?.map((product) => {
@@ -33,11 +13,7 @@ function ProductCardGen(props) {
             key={product.productId}
             {...product}
             imageUrl={breakUrl}
-            setWishlists={saveWishlists(
-              product.productId,
-              product.categoryName
-            )}
-            wishlists={wishlists}
+            wishlists="true"
           />
         );
       })}
