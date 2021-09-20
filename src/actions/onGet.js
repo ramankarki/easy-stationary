8b0685cd398f088fbfa615fd4c3fb98845bf2af7
ramankarki <ramankarki40@gmail.com>
2021-09-21 00:06:36 +0545

    save token in locaStorage if exists

--- src/actions/onGet.js
+++ src/actions/onGet.js
@@ -25,8 +25,14 @@ const onGet =
       .then(({ data }) =>
         batch(() => {
           if (appState.domainState === 'USER') {
-            const user = JSON.parse(localStorage.getItem('USER'));
+            const user = JSON.parse(localStorage.getItem('USER')) || {};
             user.user = data.user;
+
+            if (data.token) {
+              user.token = data.token;
+              user.expiryDate = Date.now() + 7776000000 + '';
+            }
+
             localStorage.setItem('USER', JSON.stringify(user));
           }
 
