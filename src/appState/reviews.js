58af591e5d3d9c38e4ea4751e27ded4210a78156
ramankarki <ramankarki40@gmail.com>
2021-09-22 20:18:08 +0545

    add condition to return route for getting users review or single product reviews

--- src/appState/reviews.js
+++ src/appState/reviews.js
@@ -4,8 +4,10 @@ import getErrorTag from '../utils/errorTags';
 const reviews = {
   [APP_REVIEWS_STATE]: {
     postRoute: () => `/api/v1/reviews`,
-    getRoute: (productId, page, sort) =>
-      `/api/v1/reviews/product/${productId}?page=${page}&limit=9&sort=${sort}`,
+    getRoute: (productId, page, sort, userReview) =>
+      userReview
+        ? `/api/v1/reviews/user?page=${page}&limit=9&sort=${sort}`
+        : `/api/v1/reviews/product/${productId}?page=${page}&limit=9&sort=${sort}`,
     modalMsg: (requestStatus, errorTag) => {
       switch (requestStatus) {
         case 'postSuccess':
