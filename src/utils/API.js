348bf53f502cbb9c77d5c7cb32fa71128cde412e
ramankarki <ramankarki40@gmail.com>
2021-09-22 20:22:30 +0545

    separate baseURL for dev and prod

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,11 @@
 import { create } from 'axios';
 
 function API() {
+  let baseURL = process.env.REACT_APP_API_1;
+  if (process.env.NODE_ENV !== 'production') baseURL = 'http://localhost:8000';
+
   return create({
-    baseURL: process.env.REACT_APP_API_1,
+    baseURL,
     headers: {
       authorization:
         'Bearer ' + JSON.parse(localStorage.getItem('USER'))?.token,
