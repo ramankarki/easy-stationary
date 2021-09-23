4b2d0af389fe8c21d91be304ec6df36700cd16fe
ramankarki <ramankarki40@gmail.com>
2021-09-23 23:40:40 +0545

    add private prop in each obj for mobile navigation btn

--- src/templates/ClientDashboard/asideButtonsData.js
+++ src/templates/ClientDashboard/asideButtonsData.js
@@ -11,26 +11,31 @@ const aside = [
     value: 'Dashboard',
     path: DASHBOARD,
     iconsrc: '/assets/overview icon.svg',
+    private: true,
   },
   {
     value: 'Orders',
     path: DASHBOARD_ORDERS,
     iconsrc: '/assets/order icon.svg',
+    private: true,
   },
   {
     value: 'My reviews',
     path: DASHBOARD_MY_REVIEWS,
     iconsrc: '/assets/my reviews.svg',
+    private: true,
   },
   {
     value: 'My wishlists',
     path: DASHBOARD_MY_WISHLISTS,
     iconsrc: '/assets/my wishlists.svg',
+    private: true,
   },
   {
     value: 'Settings',
     path: DASHBOARD_SETTINGS,
     iconsrc: '/assets/settings icon.svg',
+    private: true,
   },
 ];
 
