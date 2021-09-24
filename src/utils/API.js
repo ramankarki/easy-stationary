757bf7f5f2833c0c28b149abf29546905e8c372c
ramankarki <ramankarki40@gmail.com>
2021-09-24 19:47:03 +0545

    request from backup api if needed

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,10 @@ import { create } from 'axios';
 
 function API() {
   let baseURL = process.env.REACT_APP_API_1;
+
+  const day = new Date().getDate();
+  if (day === 1 || day > 20) baseURL = process.env.REACT_APP_API_2;
+
   if (process.env.NODE_ENV !== 'production') baseURL = 'http://localhost:8000';
 
   return create({
