8306db46b8df47c63bf928685936303c23b641c2
ramankarki <ramankarki40@gmail.com>
2021-10-09 21:22:22 +0545

    don't show .reviewsCards if empty

--- src/pages/SingleProduct/index.jsx
+++ src/pages/SingleProduct/index.jsx
@@ -372,7 +372,7 @@ function SingleProduct(props) {
           )}
 
           {/* reviews cards */}
-          {props.reviews && (
+          {props.reviews?.length > 0 && (
             <div className="reviewsCards">
               {props.reviews?.map((review) => (
                 <div key={review._id} className="reviewsCards__card">
