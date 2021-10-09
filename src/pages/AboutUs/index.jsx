24acd68f18faa5643701c3e3b7fa9c94005cfe16
ramankarki <ramankarki40@gmail.com>
2021-10-09 18:20:03 +0545

    about us dummy page

---
+++ src/pages/AboutUs/index.jsx
@@ -0,0 +1,14 @@
+import Header from '../../templates/Header';
+
+import './aboutUs.scss';
+
+function AboutUs() {
+  return (
+    <div className="AboutUs">
+      <Header />
+      <h1>About us</h1>
+    </div>
+  );
+}
+
+export default AboutUs;
