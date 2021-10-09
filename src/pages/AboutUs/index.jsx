0cac1b7afd59dd58f6fec96df18b230d67c97a47
ramankarki <ramankarki40@gmail.com>
2021-10-09 20:55:16 +0545

    scroll to top on mount

--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import Header from '../../templates/Header';
 import BreadCrumb from '../../components/BreadCrumb';
 import Footer from '../../templates/Footer';
@@ -5,6 +7,10 @@ import Footer from '../../templates/Footer';
 import './aboutUs.scss';
 
 function AboutUs() {
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+  }, []);
+
   return (
     <div className="AboutUs">
       <Header />
